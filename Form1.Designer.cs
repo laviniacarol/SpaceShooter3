aa7cc83f3f522cbb5941d31c5d2e60ae447f126e
laviniacarol <carolxlavinia@gmail.com>
2026-01-25 19:06:15 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace SpaceShooter3
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        /// name="disposing">
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+     
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Player = new PictureBox();
+            LeftMoveTimer = new System.Windows.Forms.Timer(components);
+            RightMoveTimer = new System.Windows.Forms.Timer(components);
+            UpMoveTimer = new System.Windows.Forms.Timer(components);
+            DownMoveTimer = new System.Windows.Forms.Timer(components);
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)Player).BeginInit();
+            SuspendLayout();
+          
+            Player.BackColor = Color.Transparent;
+            Player.BackgroundImage = (Image)resources.GetObject("Player.BackgroundImage");
+            Player.Location = new Point(226, 356);
+            Player.Name = "Player";
+            Player.Size = new Size(96, 94);
+            Player.TabIndex = 0;
+            Player.TabStop = false;
+            Player.Click += pictureBox1_Click;
+         
+            LeftMoveTimer.Tick += LeftMoveTimer_Tick;
+           
+            RightMoveTimer.Tick += RightMoveTimer_Tick;
+           
+            UpMoveTimer.Tick += UpMoveTimer_Tick;
+           
+            
+            DownMoveTimer.Tick += DownMoveTimer_Tick;
+         
+            timer1.Tick += MoveBgTimer;
+        
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.FromArgb(0, 0, 64);
+            ClientSize = new Size(582, 450);
+            Controls.Add(Player);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximumSize = new Size(600, 500);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)Player).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox Player;
+        private System.Windows.Forms.Timer LeftMoveTimer;
+        private System.Windows.Forms.Timer RightMoveTimer;
+        private System.Windows.Forms.Timer UpMoveTimer;
+        private System.Windows.Forms.Timer DownMoveTimer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SpaceShooter3
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] stars;
+        int backgroundspeed;
+        Random rnd;
+        int playerSpeed = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+            this.KeyUp += Form1_KeyUp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundspeed = 4;
+            stars = new PictureBox[10];
+            rnd = new Random();
+
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new PictureBox();
+                stars[i].BorderStyle = BorderStyle.None;
+                stars[i].Location = new Point(rnd.Next(20, 500), rnd.Next(-10, 400));
+
+                if (i % 2 == 1)
+                {
+                    stars[i].Size = new Size(2, 2);
+                    stars[i].BackColor = Color.Wheat;
+                }
+                else
+                {
+                    stars[i].Size = new Size(3, 3);
+                    stars[i].BackColor = Color.DarkGray;
+                }
+
+                this.Controls.Add(stars[i]);
+            }
+
+            MoveBgTimer.Start();
+        }
+
+        private void MoveBgTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i].Top += backgroundspeed;
+                if (stars[i].Top >= this.Height)
+                    stars[i].Top = -stars[i].Height;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) LeftMoveTimer.Start();
+            if (e.KeyCode == Keys.Right) RightMoveTimer.Start();
+            if (e.KeyCode == Keys.Up) UpMoveTimer.Start();
+            if (e.KeyCode == Keys.Down) DownMoveTimer.Start();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) LeftMoveTimer.Stop();
+            if (e.KeyCode == Keys.Right) RightMoveTimer.Stop();
+            if (e.KeyCode == Keys.Up) UpMoveTimer.Stop();
+            if (e.KeyCode == Keys.Down) DownMoveTimer.Stop();
+        }
+
+        private void LeftMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Left > 10)
+                Player.Left -= playerSpeed;
+        }
+
+        private void RightMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Right < 500)
+                Player.Left += playerSpeed;
+        }
+
+        private void DownMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top < 400)
+                Player.Top += playerSpeed;
+        }
+
+        private void UpMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top > 10)
+                Player.Top -= playerSpeed;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void MoveBgTimer(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
